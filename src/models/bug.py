49b578fa107233c2fe10ad1b4869c32f4e21ff37
Rafał <steczkiewicz1998@Gmail.com>
2026-08-21 11:47:17 +0200

    feat: add User and Bug data models

---
+++ src/models/bug.py
@@ -0,0 +1,77 @@
+class Bug:
+    def __init__(
+        self,
+        title="title",
+        bug_id=0,
+        status="status",
+        priority="priority",
+        description="description",
+        assigned_to="assigned",
+        reported_by="reported",
+    ):
+        self.__title = title
+        self.__bug_id = bug_id
+        self.__status = status
+        self.__priority = priority
+        self.__description = description
+        self.__assigned_to = assigned_to
+        self.__reported_by = reported_by
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def bug_id(self):
+        return self.__bug_id
+
+    @property
+    def reported_by(self):
+        return self.__reported_by
+
+    @property
+    def status(self):
+        return self.__status
+
+    @status.setter
+    def status(self, new_status):
+        self.__status = new_status
+
+    @property
+    def priority(self):
+        return self.__priority
+
+    @priority.setter
+    def priority(self, new_priority):
+        self.__priority = new_priority
+
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, new_description):
+        self.__description = new_description
+
+    @property
+    def assigned_to(self):
+        return self.__assigned_to
+
+    @assigned_to.setter
+    def assigned_to(self, new_assigned_to):
+        self.__assigned_to = new_assigned_to
+
+    def __repr__(self):
+        return (
+            f"Title: {self.__title}\n"
+            f"Bug ID: {self.__bug_id}\n"
+            f"Status: {self.__status}\n"
+            f"Priority: {self.__priority}\n"
+            f"Description: {self.__description}\n"
+            f"Assigned to: {self.__assigned_to}\n"
+            f"Reported by: {self.__reported_by}"
+        )
+
+
+first_bug = Bug(title="shakaka")
+print(first_bug)
---
+++ src/models/user.py
@@ -0,0 +1,49 @@
+class User:
+    allowed_positions = ["QA", "Worker", "Galaxy Destroyer"]  # noqa: RUF012
+
+    def __init__(
+        self,
+        name="name",
+        last_name="last_name",
+        employee_id=0,
+        employed=True,
+        position="Worker",
+    ):
+        self.__name = name
+        self.__last_name = last_name
+        self.__id = employee_id
+        self.__employed = employed
+        self.__position = position
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, new_position):
+        if new_position in self.allowed_positions:
+            self.__position = new_position
+        else:
+            print("Picked position is not available")
+
+    @property
+    def employed(self):
+        return self.__employed
+
+    @employed.setter
+    def employed(self, employed_status):
+        if employed_status == "yes":
+            self.__employed = True
+        else:
+            self.__employed = False
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, new_last_name):
+        self.__last_name = new_last_name
+
+    def __repr__(self):
+        return f"Name: {self.__name}\nLast name: {self.__last_name}\nid: {self.__id}\nemployed: {self.__employed}\nposition: {self.__position}"
